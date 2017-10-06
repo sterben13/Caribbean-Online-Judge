fb5c04732b229d2b52922b0887740a3ae99803ae
Ivan Romero Garcia <ivan.dev.job@gmail.com>
2017-10-06 18:44:12 -0500

    Add files via upload

---
+++ src/Main3932.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Main3932
+ */
+public class Main3932 {
+
+    public static void main(String[] args) {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        OutputStreamWriter osw = new OutputStreamWriter(System.out);
+        BufferedWriter bw = new BufferedWriter(osw);
+        int n;
+        String line;
+        int [] number
+        try {
+            n = Integer.parseInt(br.readLine());
+            while (n-- > 0) {
+                
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+}
